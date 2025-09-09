d6fd072fd6a6b6bc9867231591aacd3fcbb00d88
MARCOS ANDRÉ <marcos.andre101620@gmail.com>
2025-09-09 03:23:43 +0000

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int carta1(){
+int carta1(A){
+    
     int main();
     char nome[] = "Brasil";
     char Estado[] = "Ceará";
-    char codigo[][5] = {"A01", "A02", "A03", "A04"};
+    char codigo[][4] = {"A01", "B02", "C03", "D04"};
     char cidade[] = "Fortaleza";
     int população = 2000000;
-    float área_km2 = 10.0000000000;
+    float área (km²) = 10.0000000000;
     float PIB = 30.0000000000;
     int Numero_pontos_turisticos = 50;
 
@@ -16,22 +17,24 @@ int carta1(){
     printf("Código: %s\n", codigo[0]);
     printf("Cidade: %s\n", cidade);
     printf("População: %d\n", população);
-    printf("Área (km²): %.10f\n", área_km2);
+    printf("Área (km²): %.10f\n", área ("km²");
     printf("PIB: %.10f\n", PIB);
     printf("Número de pontos turísticos: %d\n", Numero_pontos_turisticos);
 
-     return 0;    
-    
-  
+     return 0;  
+     
+}
 
-int carta2();
-    int main();
+#include <stdio.h>
+
+int main(){
+    int carta2(B);
     char nome[] = "Brasil";
     char Estado[] = "Maranhão";
-    char codigo[][5] = {"B01", "B02", "B03", "B04"};
+    char codigo[][4] = ("A01", "B02", "C03", "D04");
     char cidade[] = "Imperatriz";
     int população = 1680000;
-    float área_km2 = 17500000;
+    float área_km²= (1200.25);
     float PIB = 30000000000;
     int Numero_pontos_turisticos = 50;
 
