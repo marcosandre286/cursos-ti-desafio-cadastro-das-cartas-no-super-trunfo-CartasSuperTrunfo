24060e6d783b43bf88a9583c8c7a8a7579a4978d
MARCOS ANDRÉ <marcos.andre101620@gmail.com>
2025-09-09 03:41:17 +0000

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,52 +1,53 @@
 #include <stdio.h>
 
-int carta1(A){
+int carta1(A){ // carta 1;
     
     int main();
-    char nome[] = "Brasil";
-    char Estado[] = "Ceará";
-    char codigo[][4] = {"A01", "B02", "C03", "D04"};
-    char cidade[] = "Fortaleza";
+    char nome = "Brasil";
+    char Estado = "Ceará";
+    char codigo[4] = {"A01", "B02", "C03", "D04"};
+    char cidade = "Fortaleza";
     int população = 2000000;
     float área_km2 = 10.0000000000;
-    float PIB = 30.0000000000;
+    float PIB = 3000.00000000;
     int Numero_pontos_turisticos = 50;
 
-    printf("País: %s\n", nome);
-    printf("Estado: %s\n", Estado);
-    printf("Código: %s\n", codigo[0]);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", população);
-    printf("Área (km²): %.10f\n", área_km2);
-    printf("PIB: %.10f\n", PIB);
-    printf("Número de pontos turísticos: %d\n", Numero_pontos_turisticos);
+    printf("País: %s\n, nome");
+    printf("Estado: %s\n, Estado");
+    printf("Código: %s\n, codigo[0]");
+    printf("Cidade: %s\n, cidade");
+    printf("População: %d\n, população");
+    printf("Área (km²): %.10f\n, área_km2");
+    printf("PIB: %.10f\n, PIB");
+    printf("Número de pontos turísticos: %d\n, Numero_pontos_turisticos");
 
      return 0;  
      
-}
+} // fim da carta 1;
 
 #include <stdio.h>
 
-int main(){
+int main(){ // carta 2;
+
     int carta2(B);
-    char nome[] = "Brasil";
-    char Estado[] = "Maranhão";
-    char codigo[][4] = ("A01", "B02", "C03", "D04");
-    char cidade[] = "Imperatriz";
+    char nome = "Brasil";
+    char Estado = "Maranhão";
+    char codigo[4] = ("A01", "B02", "C03", "D04");
+    char cidade = "Imperatriz";
     int população = 1680000;
     float área_km²= (1200.25);
     float PIB = 30000000000;
-    int Numero_pontos_turisticos = 50;
+    int Numero_pontos_turisticos = 35;
 
-    printf("País: %s\n", nome);
-    printf("Estado: %s\n", Estado);
-    printf("Código: %s\n", codigo[0]);
-    printf("Cidade: %s\n", cidade);
-    printf("População: %d\n", população);
-    printf("Área (km²): %.10f\n", área_km2);
+    printf("País: %s\n, nome");
+    printf("Estado: %s\n, Estado");
+    printf("Código: %s\n, codigo[0]");
+    printf("Cidade: %s\n, cidade");
+    printf("População: %d\n, população");
+    printf("Área (km²): %.10f\n, area_km2");
     printf("PIB: %.10f\n", PIB);
-    printf("Número de pontos turísticos: %d\n", Numero_pontos_turisticos);
+    printf("Número de pontos turísticos: %d\n, Numero_pontos_turisticos");
 
      return 0;    
     
-} 
+} // fim da carta 2;
