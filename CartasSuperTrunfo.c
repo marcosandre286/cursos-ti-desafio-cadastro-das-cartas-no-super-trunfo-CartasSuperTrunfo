93b8c7c60b86611455f582408a5e993c99b095e7
MARCOS ANDRÉ <marcos.andre101620@gmail.com>
2025-09-09 00:22:32 +0000

    CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int carta1();
-    int main(){
+int carta1(){
+    int main();
     char nome[] = "Brasil";
     char Estado[] = "Ceará";
     char codigo[][5] = {"A01", "A02", "A03", "A04"};
@@ -21,6 +21,29 @@ int carta1();
     printf("Número de pontos turísticos: %d\n", Numero_pontos_turisticos);
 
      return 0;    
+    
+  
 
-}   
+int carta2();
+    int main();
+    char nome[] = "Brasil";
+    char Estado[] = "Maranhão";
+    char codigo[][5] = {"B01", "B02", "B03", "B04"};
+    char cidade[] = "Imperatriz";
+    int população = 1680000;
+    float área_km2 = 17500000;
+    float PIB = 35000000;
+    int Numero_pontos_turisticos = 35;
 
+    printf("País: %s\n", nome);
+    printf("Estado: %s\n", Estado);
+    printf("Código: %s\n", codigo[0]);
+    printf("Cidade: %s\n", cidade);
+    printf("População: %d\n", população);
+    printf("Área (km²): %.10f\n", área_km2);
+    printf("PIB: %.10f\n", PIB);
+    printf("Número de pontos turísticos: %d\n", Numero_pontos_turisticos);
+
+     return 0;    
+    
+} 
